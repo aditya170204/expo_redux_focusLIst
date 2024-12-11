65b8ca5f3936d7f6c51a88bf0ff60f12a32649e1
aditya170204 <adityaraj17feb@gmail.com>
2024-12-11 12:09:06 +0530

    added some basic styling in TargetList

--- src/Component/TargetList.js
+++ src/Component/TargetList.js
@@ -14,17 +14,34 @@ const TargetList = ({ navigation }) => {
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <View style={styles.target}>
-            <Text>{item.text}</Text>
-            <Button
-              title="Edit"
-              onPress={() => navigation.navigate("EditTarget", { id: item.id })}
-            />
-            <Button
-              title="Delete"
-              onPress={() =>
-                navigation.navigate("DeleteTarget", { id: item.id })
-              }
-            />
+            <Text style={styles.targetText}>{item.text}</Text>
+            <View
+              style={{
+                // justifyContent: "space-evenly",
+                flexDirection: "row",
+                margin: 30,
+                width: "45%",
+                gap: 10,
+                // backgroundColor: "red",
+                // padding: 10,
+                // borderRightColor,
+              }}
+            >
+              <Button
+                style={styles.editButton}
+                title="Edit"
+                onPress={() =>
+                  navigation.navigate("EditTarget", { id: item.id })
+                }
+              />
+              <Button
+                title="Delete"
+                onPress={() =>
+                  navigation.navigate("DeleteTarget", { id: item.id })
+                }
+                color={"#B02E0C"}
+              />
+            </View>
           </View>
         )}
       />
@@ -40,6 +57,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    backgroundColor: "grey",
   },
   heading: {
     fontSize: 24,
@@ -47,9 +65,24 @@ const styles = StyleSheet.create({
   },
   target: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    marginVertical: 10,
+    justifyContent: "space-around",
+    marginVertical: 13,
+    backgroundColor: "#000001",
+    alignItems: "center",
+    borderRadius: 20,
   },
+  targetText: {
+    color: "white",
+    fontSize: 17,
+    width: "55%",
+
+    // backgroundColor: "yellow",
+    padding: 10,
+  },
+  editButton: {
+    // marginRight:
+  },
+  deleteButton: {},
 });
 
 export default TargetList;
